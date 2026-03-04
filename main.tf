371c578b75726fea40880328cd0578a749a3afd1
yash kapse <kapseyash@gmail.com>
2026-03-04 19:50:20 +0530

    new pipeline

--- main.tf
+++ main.tf
@@ -1,149 +1,19 @@
-terraform {
-  required_version = ">= 1.3"
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "19.0.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.26.0"
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.29"
+
+  subnet_ids = ["subnet-00e26a0e772435c72", "subnet-032092d1677fe84f6"]
+  vpc_id     = "vpc-0a3f4db7781994c0b"
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t2.medium"]
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
     }
   }
-
-  backend "s3" {
-    bucket  = "my-jenkins-bucket020345"
-    key     = "terraform.tfstate"
-    region  = "eu-north-1"
-    encrypt = true
-  }
-}
-
-#################################
-# PROVIDER
-#################################
-
-provider "aws" {
-  region = "eu-north-1"
-}
-
-#################################
-# DATA SOURCES
-#################################
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-#################################
-# VARIABLES
-#################################
-
-variable "cluster_name" {
-  type    = string
-  default = "my-cluster-23"
-}
-
-#################################
-# IAM ROLE FOR EKS CLUSTER
-#################################
-
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "eks.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-#################################
-# IAM ROLE FOR NODE GROUP
-#################################
-
-resource "aws_iam_role" "node_role" {
-  name = "eks-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "node_policies" {
-  count = 3
-  role  = aws_iam_role.node_role.name
-
-  policy_arn = element([
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  ], count.index)
-}
-
-#################################
-# EKS CLUSTER
-#################################
-
-resource "aws_eks_cluster" "mycluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = data.aws_subnets.default.ids
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_policy
-  ]
-}
-
-#################################
-# NODE GROUP
-#################################
-
-resource "aws_eks_node_group" "nodegroup" {
-  cluster_name    = aws_eks_cluster.mycluster.name
-  node_group_name = "myb23-node-group"
-  node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids      = data.aws_subnets.default.ids
-
-  scaling_config {
-    desired_size = 2
-    min_size     = 1
-    max_size     = 3
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.node_policies
-  ]
-}
-
-#################################
-# OUTPUTS
-#################################
-
-output "cluster_name" {
-  value = aws_eks_cluster.mycluster.name
-}
-
-output "cluster_endpoint" {
-  value = aws_eks_cluster.mycluster.endpoint
 }
