00525cf5eee84d60984e3fabe7588d3515c61d0e
yash kapse <kapseyash@gmail.com>
2026-03-05 00:24:22 +0530

    update pipeline

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "19.0.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
 
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.29"
@@ -10,10 +10,11 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      instance_types = ["t2.medium"]
+      desired_size = 2
+      max_size     = 3
       min_size     = 1
-      max_size     = 2
-      desired_size = 1
+
+      instance_types = ["t3.medium"]
     }
   }
 }
