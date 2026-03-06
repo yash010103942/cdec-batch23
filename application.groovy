92e0d1ff77afd7e7cbc24bf225a39649dcd38a18
yash kapse <kapseyash@gmail.com>
2026-03-06 11:50:23 +0530

    mypipeline

--- application.groovy
+++ application.groovy
@@ -1,58 +1,53 @@
 pipeline {
-agent any
+    agent any
 
-```
-stages {
+    stages {
 
-    stage('PULL') {
-        steps {
-            git 'https://github.com/chetanraval07/jenkins-final26.git'
+        stage('PULL') {
+            steps {
+               git 'https://github.com/yash010103942/cdec-b23-infra.git'
+            }
+        }
+
+        stage('FRONTEND-DOCKER-BUILD') {
+            steps {
+                sh '''
+                cd frontend
+                docker build -t yashkapse01/easy-frontend:latest .
+                '''
+            }
+        }
+
+        stage('BACKEND-DOCKER-BUILD') {
+            steps {
+                sh '''
+                cd backend
+                docker build -t yashkapse01/easy-backend:latest .
+                '''
+            }
+        }
+
+        stage('DOCKER-PUSH') {
+            steps {
+                sh '''
+                docker push yashkapse01/easy-frontend:latest
+                docker push yashkapse01/easy-backend:latest
+                '''
+            }
+	}
+            stage('DOCKER-CLEAN') {
+            steps {
+                sh '''
+                docker rmi -f yashkapse01/easy-frontend:latest
+                docker rmi  -f yashkpase01/easy-backend:latest
+                '''
+            }
+        }
+
+        stage('DEPLOY') {
+            steps {
+                sh 'kubectl apply -f simple-deploy/'
+            }
         }
     }
-
-    stage('FRONTEND-DOCKER-BUILD') {
-        steps {
-            sh '''
-            cd frontend
-            docker build -t shivansh7310/easy-frontend:latest .
-            '''
-        }
-    }
-
-    stage('BACKEND-DOCKER-BUILD') {
-        steps {
-            sh '''
-            cd backend
-            docker build -t shivansh7310/easy-backend:latest .
-            '''
-        }
-    }
-
-    stage('DOCKER-PUSH') {
-        steps {
-            sh '''
-            docker push shivansh7310/easy-frontend:latest
-            docker push shivansh7310/easy-backend:latest
-            '''
-        }
-    }
-
-    stage('DOCKER-CLEAN') {
-        steps {
-            sh '''
-            docker rmi -f shivansh7310/easy-frontend:latest
-            docker rmi -f shivansh7310/easy-backend:latest
-            '''
-        }
-    }
-
-    stage('DEPLOY') {
-        steps {
-            sh 'kubectl apply -f simple-deploy/'
-        }
-    }
-
-}
-```
-
 }
